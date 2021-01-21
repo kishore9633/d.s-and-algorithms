e93ec1af4e2df964105c02adab198541ef4c7762
shivakishore chowdary <48004087+kishore9633@users.noreply.github.com>
2021-01-21 13:57:34 +0530

    Add files via upload

--- s.js
+++ s.js
@@ -1,38 +1,38 @@
-
- var twoSum = function(nums, target) {
-    
-     let result=[];
-     for (var i=0; i<=nums.length; i++){
-        
-         for(var j =i+1; j<nums.length; j++){
-
-            
-             if(nums[i]+nums[j]==target){
-                 result.push(i);
-                 result.push(j);
-             }
-         }
-     }
-     return result;
-    
- };
- console.log(twoSum([2,7,11,15],9));
-const humans = {
-    age:10,
-    grow:()=>{
-        this.age++;
-
-    },
-
-}
-humans.grow();
-console.log(humans.age);
-let a = [1,2,3,4,5,6,7];
-let b=[];
-for (var i =0; i<=a.length;i++){
-  b=b+a[i];
-}
-console.log(b);
+//
+// var twoSum = function(nums, target) {
+//    
+//     let result=[];
+//     for (var i=0; i<=nums.length; i++){
+//        
+//         for(var j =i+1; j<nums.length; j++){
+//
+//            
+//             if(nums[i]+nums[j]==target){
+//                 result.push(i);
+//                 result.push(j);
+//             }
+//         }
+//     }
+//     return result;
+//    
+// };
+// console.log(twoSum([2,7,11,15],9));
+//const humans = {
+//    age:10,
+//    grow:()=>{
+//        this.age++;
+//
+//    },
+//
+//}
+//humans.grow();
+//console.log(humans.age);
+//let a = [1,2,3,4,5,6,7];
+//let b=[];
+//for (var i =0; i<=a.length;i++){
+//  b=b+a[i];
+//}
+//console.log(b);
 const number = parseInt(prompt('Enter the number of terms: '));
 
 function fibonacci(number) {
